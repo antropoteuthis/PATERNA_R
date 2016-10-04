abd8a9a9d9d6ded26a04b34122caa17e6053d44d
antropoteuthis <alejandro_damianserrano@brown.edu>
2016-10-04 16:41:29 -0400

    2 functions in one Rscript

--- paterna/R/packagename.R
+++ paterna/R/packagename.R
@@ -4,7 +4,19 @@
 #' @param None.
 #' @keywords Name
 #' @export
-#' @examples
+#' @examples packagename()
 #' Type packagename() into the console to expand the acronym.
 
 packagename <- function(){ print("Welcome to the R package Phylogenetic Analysis of Topology in Ecological Relationship Networks and Associations.")}
+
+#' A function to state the acronym of the package
+#' @name packageacro
+#' @description A function that states the acronym of the package
+#' @param None.
+#' @keywords Name
+#' @export
+#' @examples packageacro()
+#' packageacro()
+#' "PATERNA"
+
+packageacro <- function({print("PATERNA")})
