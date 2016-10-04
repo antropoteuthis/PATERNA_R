75d4d76fded6f217f16616f8fa8ecf8bdadc3569
antropoteuthis <alejandro_damianserrano@brown.edu>
2016-10-04 11:12:42 -0400

    Added package paterna

---
+++ paterna/R/packagename.R
@@ -0,0 +1,10 @@
+#' A function to state the name of the package
+#' @name packagename
+#' @description A function that states the name of the package
+#' @param None.
+#' @keywords Name
+#' @export
+#' @examples
+#' Type packagename() into the console to expand the acronym.
+
+packagename <- function(){ print("Welcome to the R package Phylogenetic Analysis of Topology in Ecological Relationship Networks and Associations.")}
